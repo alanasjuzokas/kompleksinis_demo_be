5ad92c313d362d2b8394eab9ea6b460c8bd46104
Alanas JUZOKAS <alanas@inventi.lt>
2020-05-10 11:51:20 +0300

    feat: add default cons

--- src/main/java/bakalauras/demo/web/domain/PollRequest.java
+++ src/main/java/bakalauras/demo/web/domain/PollRequest.java
@@ -3,6 +3,8 @@ package bakalauras.demo.web.domain;
 public class PollRequest {
     public String pollId;
 
+    public PollRequest() {}
+
     public PollRequest(String pollId) {
         this.pollId = pollId;
     }
